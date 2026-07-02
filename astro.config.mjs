02c2633e1f5b16b333c23ad5502f361d0920e5bf
Rainer Gonzaga <rainerdgonzaga@gmail.com>
2026-07-02 23:55:23 +0800

    fix: finalize layout components and redirect root to voyager

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [mdx(), react(), icon()],
+  integrations: [mdx(), react(), icon(), tailwind()],
   site: 'https://jrgo7.github.io',
   base: 'virtual-exhibit-template',
+  redirects: {
+    '/': '/voyager'
+  }
 });
-
