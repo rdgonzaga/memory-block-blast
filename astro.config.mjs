ba4c1d59c71882dccd8814b4cf495f7d82511f34
Rainer Gonzaga <rainerdgonzaga@gmail.com>
2026-07-02 23:14:55 +0800

    Revert "Feat 1/layout"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [mdx(), react(), icon(), tailwind()],
+  integrations: [mdx(), react(), icon()],
   site: 'https://jrgo7.github.io',
   base: 'virtual-exhibit-template',
 });
